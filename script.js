9ce339ea185e888f788c6e73824ae40808742428
Ahmed Ali <a.husseinzaki@gmail.com>
2016-04-28 09:17:19 -0700

    refactor: Reorganize classes to follow MVC pattern

--- script.js
+++ script.js
@@ -1,106 +1,106 @@
-var Cat = function(id, name, img) {
+var Cat = function(name, img) {
     this.name = name;
-    this.id = id;
     this.img = img;
     this.clicks = 0;
-
-    // Initialize DOM Tree with Cat Information.
-    var catImgElem = this._init();
-    var self = this;
-    // Listen to click event on the cat image
-    catImgElem.addEventListener('click', function() {
-        self._clicked();
-    });
 }
 
-// Construct DOM elements for the cat
-// Element Should look like this:
-// <div>
-//     <div>Cat Name</div>
-//     <img src="cat_image_file" alt="Cat Name">
-//     <div>Number of Clicks <span id="some_id">NumOfClicks</span>
-// </div>
-//
-// Returns: The Image to be listened to when clicked.
-Cat.prototype._init = function() {
-    var catDiv = document.createElement('div');
-    catDiv.id = 'cat_' + this.id;
-    var name = document.createElement('div');
-    name.innerHTML = this.name;
-    catDiv.appendChild(name);
-    catDiv.style.display = 'none';
-    var img = document.createElement('img');
-    img.setAttribute('src', this.img);
-    img.setAttribute('alt', this.name);
-    catDiv.appendChild(img);
-
-    var clicksDiv = document.createElement('div');
-    var clicksText = document.createTextNode('Number of Clicks ');
-    var clicksCount = document.createElement('span');
-    clicksCount.id = 'click_' + this.id;
-    clicksCount.innerHTML = this.clicks;
-    clicksDiv.appendChild(clicksText);
-    clicksDiv.appendChild(clicksCount);
-    catDiv.appendChild(clicksDiv);
-
-    document.body.appendChild(catDiv);
-    return img;
-}
-
-// handler for the clicked event of the Cat object
-Cat.prototype._clicked = function() {
-    this.clicks++;
-    var catElem = document.getElementById('click_' + this.id);
-    catElem.innerHTML = this.clicks;
-};
-
-// Class for Cat List Generator
-var CatList = function(names, images) {
+// Manager that controls flow od data between views and model
+var CatManager = function() {
     this.cats = [];
-    for(var i=0; i < names.length; i++) {
-        this.cats.push(new Cat(i, names[i], images[i]));
-    }
-
-    this.selectedIndex = -1;
-    // Initialize Cat List
-    this._init();
-}
-
-// Initialize the Ordered List of Cats and add event listeners.
-CatList.prototype._init = function() {
-    var listElem = document.getElementById('catsList');
-    var self = this;
-    this.cats.forEach(function(cat) {
-        var item = document.createElement('li');
-        item.textContent = cat.name;
-        listElem.appendChild(item);
-        item.addEventListener('click', function() {
-            self._renderCat(cat);
-        });
-    });
-}
-
-// Renders the cat details
-CatList.prototype._renderCat = function(cat) {
-    if(this.selectedIndex >= 0) {
-        // Hide the current cat.
-        this._hideCat(this.cats[this.selectedIndex])
-    }
-    if(cat) {
-        console.log(cat);
-        var catElem = document.getElementById('cat_' + cat.id);
-        catElem.style.display = 'block';
-    }
-    this.selectedIndex = cat ? cat.id : -1;
+    this.selected = -1;
+    this.listView = new ListView(this);
+    this.detailView = new DetailView(this);
 };
 
-// Hide the cat div if another cat is selected
-CatList.prototype._hideCat = function(cat) {
-    if(cat) {
-        var catElem = document.getElementById('cat_' + cat.id);
-        catElem.style.display = 'none';
-    }
+CatManager.prototype.add = function(name, pic) {
+    this.cats.push(new Cat(name, pic));
+    // this.listView.init(this.cats);
+};
+
+CatManager.prototype.init = function() {
+    var catNames = this.cats.map(function(cat) {
+        return cat.name;
+    })
+    this.listView.init(catNames);
+    this.detailView.init();
+};
+
+// The callback to be called when list item is clicked.
+CatManager.prototype.listCallback = function(index) {
+    this.selected = index;
+    this.detailView.render();
+};
+
+CatManager.prototype.detailsCallback = function() {
+    this.cats[this.selected].clicks++;
 }
+
+// Returns the currently selected cat object.
+CatManager.prototype.getCurrentCat = function() {
+    if(this.selected < 0 || this.selected >= this.cats.length) {
+        return null;
+    }
+
+    return this.cats[this.selected];
+}
+
+
+// The view responsible for constructing and displaying the list
+var ListView = function(controller) {
+    this.listElem = document.getElementById('catsList');
+    this.controller = controller;
+}
+
+// Construct the HTML List of cat names and set items listeners to the callback function
+ListView.prototype.init = function(names) {
+    var index = 0;
+    var self = this;
+    this.listElem.innerHTML = '';
+    names.forEach(function(name) {
+        var item = document.createElement('li');
+        item.textContent = name;
+        self.listElem.appendChild(item);
+        item.addEventListener('click', (function(curIndex, controller) {
+            return function() {
+                controller.listCallback(curIndex);
+            };
+        })(index, self.controller));
+        index++;
+    });
+};
+
+// The view responsible for rendering the Cat details
+var DetailView = function(controller) {
+    this.detailsElem = document.getElementById('details');
+    this.nameElem = document.getElementById('name');
+    this.picElem = document.getElementById('pic');
+    this.clicksElem = document.getElementById('clicks');
+    this.controller = controller;
+}
+
+// Initially hide the details section and attach listener to click event
+DetailView.prototype.init = function() {
+    this.detailsElem.style.display = 'none';
+    this.picElem.addEventListener('click', (function(controller) {
+        return function() {
+            controller.detailsCallback();
+        };
+    })(this.controller));
+};
+
+// Render the details into the details section
+DetailView.prototype.render = function() {
+    var cat = this.controller.getCurrentCat();
+    if(cat) {
+        this.detailsElem.style.display = 'block';
+        this.nameElem.textContent = cat.name;
+        this.picElem.setAttribute('src', cat.img);
+        this.picElem.setAttribute('alt', cat.name);
+        this.clicksElem.textContent = cat.clicks;
+    } else {
+        this.detailsElem.style.display = 'none';
+    }
+};
 
 // Main Entry Point to Creating and handling the cats list.
 function initPage() {
@@ -112,7 +112,12 @@ function initPage() {
         'img/pyramids.jpg'
     ];
     var catNames = ['First Cat', 'Second Cat', 'Itza', 'Taj Mahal', 'Pyramids of Giza'];
-    var catList = new CatList(catNames, catImages);
+    var controller = new CatManager();
+    for(var i=0; i < catImages.length; i++) {
+        controller.add(catNames[i], catImages[i]);
+    }
+
+    controller.init();
 }
 
 document.addEventListener('DOMContentLoaded', initPage, false);
