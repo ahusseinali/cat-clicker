f424ec6df74c7fc0af724834039a52491640a069
Ahmed Ali <a.husseinzaki@gmail.com>
2016-05-02 00:08:08 -0700

    feat: Enable Admin Mode binding

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ var Cat = function(name, img) {
     this.clicks = ko.observable(0);
 }
 
+Cat.prototype.copyTo = function(obj) {
+    obj.name = ko.observable(this.name());
+    obj.img = ko.observable(this.img());
+    obj.clicks = ko.observable(this.clicks());
+}
+
+Cat.prototype.copyFrom = function(obj) {
+    this.name(obj.name());
+    this.img(obj.img());
+    this.clicks(obj.clicks())
+}
+
 var CatViewModel = function() {
     this.cats = ko.observableArray([
         new Cat('First Cat', 'img/cat1.jpg'),
@@ -15,22 +27,43 @@ var CatViewModel = function() {
     this.selected = ko.observable(-1);
     this.isCatSelected = ko.computed(this.checkCatSelected, this);
     this.currentCat = ko.observable(null);
-}
+    this.isAdminOn = ko.observable(false);
+    this.adminCat = ko.observable(null);
+
+};
 
 CatViewModel.prototype.checkCatSelected = function() {
     return this.selected() > -1;
-}
+};
 
 CatViewModel.prototype.selectCat = function(index) {
     this.selected(index);
     this.currentCat(this.cats()[this.selected()]);
-}
+    var catCopy = {};
+    this.currentCat().copyTo(catCopy);
+    this.adminCat(catCopy);
+
+};
 
 CatViewModel.prototype.incrementClicks = function() {
     if(this.selected() == -1) {
         return;
     }
     this.currentCat().clicks(this.currentCat().clicks() + 1);
+    this.adminCat().clicks(this.currentCat().clicks());
+};
+
+CatViewModel.prototype.showAdmin = function() {
+    this.isAdminOn(true);
+}
+
+CatViewModel.prototype.hideAdmin = function() {
+    this.isAdminOn(false);
+}
+
+CatViewModel.prototype.saveAdmin = function() {
+    this.currentCat().copyFrom(this.adminCat());
+    this.hideAdmin();
 }
 
 
